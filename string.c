7c4df132167564481216fe808a0911f5a68d9cae
AswiniSankar <53120389+AswiniSankar@users.noreply.github.com>
2020-04-20 16:19:46 +0530

    Update string.c

--- string.c
+++ string.c
@@ -3,10 +3,9 @@
 int main()
 {
   char a[100];
-  int n,c=0,i,x=0,j;
+  int i,j;
   printf("enter the string\n");
   gets(a);
-  n=strlen(a);
   for(i=0;i<strlen(a);i++)
   {for(j=0;j<strlen(a);j++)
     {if(j==i ||(i+j)==n-1)
